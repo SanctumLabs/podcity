1868ce83d6549838982123be8d977258a773366b
BrianLusina <12752833+BrianLusina@users.noreply.github.com>
2021-11-19 18:31:09 +0300

    feat(models): episode model

--- podcasts/models.py
+++ podcasts/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Episode(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    pub_date = models.DateTimeField()
+    link = models.URLField()
+    image = models.URLField()
+    podcast_name = models.CharField(max_length=255)
+    guid = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.podcast_name}: {self.title}"
--- podcasts/tests.py
+++ podcasts/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
+from django.utils import timezone
+from .models import Episode
 
-# Create your tests here.
+
+class PodCastsTests(TestCase):
+    def setUp(self):
+        self.episode = Episode.objects.create(
+            title='Test Episode',
+            description='Look Ma! I am awesome',
+            pub_date=timezone.now(),
+            link='http://example.com/podcast/test-episode.mp3',
+            image='http://example.com/podcast/test-episode.jpg',
+            podcast_name='Test Podcast',
+            guid="i7wos89cruje89kfvyno7845yt7onjuvtm4"
+        )
+
+    def test_episode_content(self):
+        self.assertEqual(self.episode.description, 'Look Ma! I am awesome')
+        self.assertEqual(self.episode.title, 'Test Episode')
+        self.assertEqual(self.episode.link, 'http://example.com/podcast/test-episode.mp3')
+        self.assertEqual(self.episode.image, 'http://example.com/podcast/test-episode.jpg')
+        self.assertEqual(self.episode.podcast_name, 'Test Podcast')
+        self.assertEqual(self.episode.guid, "i7wos89cruje89kfvyno7845yt7onjuvtm4")
+
+    def test_episode_str_representation(self):
+        self.assertEqual(str(self.episode), 'Test Podcast: Test Episode')
